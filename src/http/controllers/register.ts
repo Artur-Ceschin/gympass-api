611afafaeeb6cf716f589cb921be0f4337489b46
Artur Ceschin <artur.ceschin@gmail.com>
2024-01-03 18:24:23 -0300

    refactor: 💡 added error handler first implementation

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { PrismaUserRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists'
 import { RegisterUseCase } from '@/use-cases/register'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
@@ -19,7 +20,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     await registerUseCase.execute({ name, email, password })
   } catch (error) {
-    return reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ error: error.message })
+    }
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
