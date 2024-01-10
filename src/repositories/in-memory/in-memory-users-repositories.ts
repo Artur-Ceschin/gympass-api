c74b1302f648fe0bc8dfd5547ac9f1388bdf8e2d
Artur Ceschin <artur.ceschin@gmail.com>
2024-01-10 14:11:06 -0300

    feat: 🎸 users profile use cases and new repository method

--- src/repositories/in-memory/in-memory-users-repositories.ts
+++ src/repositories/in-memory/in-memory-users-repositories.ts
@@ -1,8 +1,17 @@
 import { User, Prisma } from '@prisma/client'
 import { UserRepository } from '../users-repositoy'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryUsersRepository implements UserRepository {
   public items: User[] = []
+
+  async findById(id: string) {
+    const user = this.items.find((item) => item.id === id)
+
+    if (!user) return null
+
+    return user
+  }
 
   async findByEmail(email: string) {
     const user = this.items.find((item) => item.email === email)
@@ -14,7 +23,7 @@ export class InMemoryUsersRepository implements UserRepository {
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: 'user-1',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
