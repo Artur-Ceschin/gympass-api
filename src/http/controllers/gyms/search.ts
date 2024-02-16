72e666d76a7f3a5a23ba937cc3845181c63db27a
Artur Ceschin <artur.ceschin@gmail.com>
2024-02-16 12:11:30 -0300

    feat: 🎸 refactored project structure and created gyms controlle

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,24 @@
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+import { z } from 'zod'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsBodySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { page, query } = searchGymsBodySchema.parse(request.body)
+
+  const createGymUseCase = makeSearchGymsUseCase()
+
+  const { gyms } = await createGymUseCase.execute({
+    query,
+    page,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
