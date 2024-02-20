809f1d9dde24bceeff6f1dc92f848781eff22cf3
Artur Ceschin <artur.ceschin@gmail.com>
2024-02-20 18:49:53 -0300

    feat: 🎸 refresh token and token in cookies implementation

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,12 +1,16 @@
 import { FastifyInstance } from 'fastify'
+import { verifyJWT } from '@/http/middleware/verify-jwt'
+
 import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
-import { verifyJWT } from '@/http/middleware/verify-jwt'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   // Only Authenticated
   app.get('/me', { onRequest: [verifyJWT] }, profile)
